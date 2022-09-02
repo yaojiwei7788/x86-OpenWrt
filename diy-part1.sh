808acb9cccb3cd05c8446e62c3a23391423b970f
yaojiwei7788 <112696439+yaojiwei7788@users.noreply.github.com>
2022-09-03 00:12:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,10 +16,10 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+#echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+#echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+#echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+#echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #src-git nas https://github.com/linkease/nas-packages.git;master >>feeds.conf.default
